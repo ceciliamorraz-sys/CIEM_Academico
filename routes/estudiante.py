9c45e619b88ccf96bf096a90c9c9cb62e530bb76
Veronica Morraz <tucorreo@gmail.com>
2026-08-04 21:20:14 -0600

    Activa visualizacion de notas en dashboard estudiante

--- routes/estudiante.py
+++ routes/estudiante.py
@@ -28,18 +28,52 @@ def dashboard():
     conversaciones = []
     mensajes = []
 
+
     if session.get("rol") == "estudiante":
 
         estudiante = db.estudiantes.find_one({
-            "_id": session.get("id")
+            "usuario": session.get("usuario")
         })
 
+
         if estudiante:
-            notas = list(db.notas.find({
-                "estudiante_id": session.get("id")
-            }))
+
+            notas = list(
+                db.notas.find({
+                    "$or": [
+                        {
+                            "estudiante_id": estudiante.get("id")
+                        },
+                        {
+                            "estudiante_id": str(estudiante.get("_id"))
+                        }
+                    ]
+                })
+            )
+
 
     elif session.get("rol") == "padre":
+
+        estudiante = db.estudiantes.find_one({
+            "padre": session.get("usuario")
+        })
+
+
+        if estudiante:
+
+            notas = list(
+                db.notas.find({
+                    "$or": [
+                        {
+                            "estudiante_id": estudiante.get("id")
+                        },
+                        {
+                            "estudiante_id": str(estudiante.get("_id"))
+                        }
+                    ]
+                })
+            )
+
 
         mensajes = list(
             db.mensajes.find({
@@ -47,53 +81,20 @@ def dashboard():
             }).sort("fecha", -1)
         )
 
+
     docentes = list(db.docentes.find())
+
 
     return render_template(
         "estudiante/estudiante_dashboard.html",
         estudiante=estudiante,
         notas=notas,
         docentes=docentes,
-        conversaciones=conversaciones,
+        conversaciones=[],
         mensajes=mensajes,
         resumen={
             "promedio": 0,
             "asistencia": 0,
             "estado": "Activo"
         }
-    )
-# =====================================
-# NOTAS DEL ESTUDIANTE
-# =====================================
-
-@estudiante_bp.route("/notas")
-@role_required("estudiante")
-def notas():
-
-    estudiante_id = session.get("id")
-
-
-    estudiante = db.estudiantes.find_one({
-        "_id": estudiante_id
-    })
-
-
-    if not estudiante:
-        flash("Estudiante no encontrado", "danger")
-        return redirect(
-            url_for("estudiante.dashboard")
-        )
-
-
-    notas = list(
-        db.notas.find({
-            "estudiante_id": estudiante_id
-        })
-    )
-
-
-    return render_template(
-        "estudiante/notas.html",
-        estudiante=estudiante,
-        notas=notas
     )
